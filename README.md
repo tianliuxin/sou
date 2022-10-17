3a9116aba392fd3c52458ec293112ac111cd8817
tianliuxin <1627094964@qq.com>
2022-10-17 21:39:55 +0800

    add README

---
+++ README.md
@@ -0,0 +1,21 @@
+# sou的作用
+sou主要是为了命令行搜索而实现的,实现的原理是借助本地的搜索软件去打开指定的链接或者搜索指定的内容
+
+# sou的安装
+下载源码包进行`pip install 源码包名`,或者使用`python setup.py install`去安装
+
+# sou的用法
+在sou安装的地方打开config.py,在里面配置搜索软件的位置和你需要的搜索引擎即可!  
+
+```bash
+
+sou -h # 查看帮助
+sou hello,world # 搜索hello,world
+sou -l www.baidu.com # 打开百度
+sou -a google -e github nlp # 使用google在github上搜索nlp
+sou hello world # 会分别搜索hello和world,若想作为一个整体可以使用sou "hello world"
+
+```
+
+# 待办
+后面可能改变配置的方式,比如配置文件的修改或者使用命令配置等
--- sou/config.py
+++ sou/config.py
@@ -2,18 +2,18 @@
 # 搜索软件
 apps = {
     "apps":{
-        "edge":r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe',
+        "edge":r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe', # 搜索软件名和软件路径
         "google":r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
     },
-    "default":"edge"
+    "default":"edge" # 默认搜索软件
 }
 
 # 搜索引擎
 engines = {
     "engines":{
         "bing":{
-            "base_url":"https://www.cn.bing.com",
-            "search_url":"https://www.cn.bing.com/search?q="
+            "base_url":"https://www.cn.bing.com", # 引擎首页的url
+            "search_url":"https://www.cn.bing.com/search?q=" # 引擎的搜索功能url
         },
         "baidu":{
             "base_url":"https://www.baidu.com",
