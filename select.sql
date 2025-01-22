82cb578ccade1f92baf6928289f7607676a42e27
tianliuxin <1627094964@qq.com>
2025-01-22 10:13:50 +0800

    Add files via upload

---
+++ select.sql
@@ -0,0 +1,39 @@
+select
+    a0.*
+from test_data a0
+left join test_data_2 a1
+on a0.vin = a1.vin
+
+TODO:test_data
+
+info:test_data
+'''
+note:hhhh
+'''
+
+
+onenote:///C:\Users\mathartsys\Documents\OneNote%20笔记本\20240815_测试\测试分区.one#section-id={52D1550F-2649-418C-BBA9-FCF11CD17858}&end
+
+onenote:///D:\MyDownload\OneNote测试\OneNote测试\test.one#section-id={683705A1-FCA6-41CF-808A-7E5E027713BC}&end
+
+
+onenote:///C:\Users\mathartsys\Documents\OneNote%20笔记本\20240815_测试\测试分区.one#section-id={52D1550F-2649-418C-BBA9-FCF11CD17858}&end
+
+onenote:///C:\Users\mathartsys\Documents\OneNote%20笔记本\20240815_测试\测试分区.one#第7页&section-id={52D1550F-2649-418C-BBA9-FCF11CD17858}&page-id={B0933F6A-9025-4636-BE2C-5AA4A51501AB}&end
+
+onenote:///C:\Users\mathartsys\Documents\OneNote%20笔记本\20240815_测试\测试分区.one#第1页&section-id={52D1550F-2649-418C-BBA9-FCF11CD17858}&page-id={C2043A4E-9B26-4722-AE50-DD54C4A61CF7}&end
+
+newPage:      {C1774CB0-C80B-4887-A099-31C5A503F9A3}{1}{E19575719590651687453520159522011655532607251}
+pageContent:  <?xml version="1.0"?>
+<one:Page xmlns:one="http://schemas.microsoft.com/office/onenote/2013/onenote" ID="{C1774CB0-C80B-4887-A099-31C5A503F9A3}{1}{E19575719590651687453520159522011655532607251}" name="无标题页" dateTime="2024-08-15T05:57:31.000Z" lastModifiedTime="2024-08-15T05:57:31.000Z" pageLevel="1"><one:QuickStyleDef index="0" name="PageTitle" fontColor="automatic" highlightColor="automatic" font="Calibri Light" fontSize="20.0" spaceBefore="0.0" spaceAfter="0.0"/><one:PageSettings RTL="false" color="automatic"><one:PageSize><one:Automatic/></one:PageSize><one:RuleLines visible="false"/></one:PageSettings><one:Title lang="zh-CN"><one:OE author="mathartsys" authorInitials="M" lastModifiedBy="mathartsys" lastModifiedByInitials="M" creationTime="2024-08-15T05:57:31.000Z" lastModifiedTime="2024-08-15T05:57:31.000Z" objectID="{5E0B39BA-2CDA-4DC7-A4AC-9DE4861D63A7}{15}{B0}" alignment="left" quickStyleIndex="0"><one:T><![CDATA[]]></one:T></one:OE></one:Title></one:Page>
+
+
+
+onenote:///C:\Users\mathartsys\Documents\OneNote%20笔记本\20240815_测试\测试分区.one#第1页&section-id={52D1550F-2649-418C-BBA9-FCF11CD17858}&page-id={C2043A4E-9B26-4722-AE50-DD54C4A61CF7}&end
+onenote:///C:\Users\mathartsys\Documents\OneNote%20笔记本\20240815_测试\测试分区.one#第2页&section-id={52D1550F-2649-418C-BBA9-FCF11CD17858}&page-id={998FD848-8CA8-4020-9222-D6265B25C80E}&end
+
+
+
+https://xzw9m5itgf.feishu.cn/base/EU5UbaGGfazG49svvDNcpP9tnbd?table=ldxfUSf3xdoiezvr					
+
+https://xzw9m5itgf.feishu.cn/base/EU5UbaGGfazG49svvDNcpP9tnbd?table=tbl0Z2176lZs4k0P&view=vewQtF9BrW
